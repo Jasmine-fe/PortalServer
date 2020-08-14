8121b336748ff91ce5eb0f52fba53bccd33e926d
Jasmine <jasmine880809@gmail.com>
2020-08-14 15:03:01 +0800

    [ websocket ]

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import * as express from 'express';
 import * as logger from 'morgan';
 import * as path from 'path';
 import * as cookieParser from 'cookie-parser';
+import * as WebSocket from 'ws';
+import * as http from 'http';
 import { connectDB } from './database'
 import { router } from './routes/index';
 
@@ -22,32 +24,22 @@ app.use(cookieParser());
 
 app.use('/', router);
 
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-// start websocket
-const WebSocket = require('ws');
- 
-const wss = new WebSocket.Server({ port: 8000 });
- 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
- 
-  ws.send('something');
-});
 
 
 // startServer log
 const port: Number = Number(process.env.PORT) || 3000;
 const startServer = async () => {
-  await app.listen(port, () => {
+  await server.listen(port, () => {
     
     wss.on('connection', function connection(ws) {
       ws.on('message', function incoming(message) {
         console.log('received: %s', message);
       });
-     
-      ws.send('something');
+     // send IP address
+      ws.send("'127.0.0.1'");
     });
 
     console.log(`
