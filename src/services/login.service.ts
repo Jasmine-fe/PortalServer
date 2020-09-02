c737457af18b1910adac0aca23a9bb0927342619
Jasmine <jasmine880809@gmail.com>
2020-09-02 11:24:01 +0800

    [ api ] add annotation for docs

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -1,6 +1,28 @@
 import { getManager, Repository, Any } from 'typeorm';
 import { Login } from '../entities/Login';
 
+/**
+ * @swagger
+ * definitions:
+ *   Login:
+ *     type: object
+ *     properties:
+ *       id: 
+ *          type: number
+ *          description: id
+ *       username: 
+ *          type: string
+ *          description: username
+ *       loginTime:
+ *          type: string
+ *          description: loginTime
+ *       password: 
+ *          type: string
+ *          description: password
+ */
+
+
+
 
 export class LoginService {
     loginRepository: Repository<Login>;
@@ -10,8 +32,35 @@ export class LoginService {
     }
 
     /**
-       * check account
-    */
+ * @swagger
+ * /login:
+ *   get:
+ *     description: checkLogin
+ *     tags:
+ *       - login
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: username
+ *         required: true
+ *         type: string
+ *       - in: query
+ *         name: password
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: successfully checkLogin
+ *         schema:
+ *           type: object
+ *           properties:
+ *             game: 
+ *               type: object
+ *               $ref: '#/definitions/Login'
+ */
     async checkLogin(req): Promise<any> {
         // login
         const username = req.body.username;
