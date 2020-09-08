bde1ea1df0ecd2468779ca28b494f57d23c61da7
Jasmine <jasmine880809@gmail.com>
2020-09-08 17:03:37 +0800

    [ DB ] test

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,42 @@
 import { createConnection } from 'typeorm';
 
+const opt = {
+  "type": "mysql",
+  "host": "192.168.43.196",
+  "username": "kitty",
+  "password": "pink",
+  "port": "3306",
+  "database": "gamedb",
+  "timeout": 15000,
+  "connectTimeout": 15000,
+  "acquireTimeout": 15000,
+  "entities": [
+      "src/entities/*.ts"
+   ],
+   "migrations": [
+      "src/migration/*.ts"
+   ],
+   "subscribers": [
+    "src/subscriber/*.ts"
+   ]
+}
+
+
+
 export const connectDB = async () => {
-  await createConnection()
+  await createConnection({
+    type: "mysql",
+    host: "192.168.43.196",
+    port: 3306,
+    username: "kitty",
+    password: "pink",
+    database: "gamedb",
+    entities: [
+      "src/entities/*.ts"
+    ],
+    synchronize: true,
+    logging: false,
+  })
   .then(res => {
     console.log("DB connection success");
   })
