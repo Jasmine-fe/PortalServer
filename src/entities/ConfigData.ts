530523eac4f3e4cf796fcc12381eae027f145d86
Jasmine <jasmine880809@gmail.com>
2020-09-28 09:04:56 +0800

    [ DB ] renew

---
+++ src/entities/ConfigData.ts
@@ -0,0 +1,22 @@
+import { Column, Entity } from "typeorm";
+
+@Entity("config_data", { schema: "gamedb" })
+export class ConfigData {
+  @Column("int", { primary: true, name: "id" })
+  id: number;
+
+  @Column("int", { name: "gameid", nullable: true })
+  gameid: number | null;
+
+  @Column("varchar", { name: "dictionary", nullable: true, length: 100 })
+  dictionary: string | null;
+
+  @Column("varchar", { name: "GAcolumn", nullable: true, length: 255 })
+  gAcolumn: string | null;
+
+  @Column("varchar", { name: "default_value", nullable: true, length: 255 })
+  defaultValue: string | null;
+
+  @Column("varchar", { name: "new_value", nullable: true, length: 255 })
+  newValue: string | null;
+}
---
+++ src/entities/ConfigTemplate.ts
@@ -0,0 +1,19 @@
+import { Column, Entity } from "typeorm";
+
+@Entity("config_template", { schema: "gamedb" })
+export class ConfigTemplate {
+  @Column("int", { primary: true, name: "id" })
+  id: number;
+
+  @Column("varchar", { name: "dictionary", nullable: true, length: 100 })
+  dictionary: string | null;
+
+  @Column("varchar", { name: "GAcolumn", nullable: true, length: 255 })
+  gAcolumn: string | null;
+
+  @Column("varchar", { name: "default_value", nullable: true, length: 255 })
+  defaultValue: string | null;
+
+  @Column("varchar", { name: "inpath", nullable: true, length: 255 })
+  inpath: string | null;
+}
