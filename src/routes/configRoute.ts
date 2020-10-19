857e8f0bb770602e4d446355ead321cc59cb3ebe
Jasmine <jasmine880809@gmail.com>
2020-10-19 08:24:56 +0800

    [ api ] fine tune config api path

--- src/routes/configRoute.ts
+++ src/routes/configRoute.ts
@@ -3,7 +3,6 @@ import * as config from '../controllers/config.controller'
 export const configRouter: Router = Router();
 
 configRouter.get('/template', config.getConfigTemplate)
-configRouter.get('/dataList', config.getConfigDataList)
 configRouter.get('/data', config.getConfigData)
-configRouter.post('/configData/new', config.recordDataConfig)
-configRouter.post('/configData/modify', config.setDataConfig)
+configRouter.post('/data/new', config.recordDataConfig)
+configRouter.post('/data/modify', config.setDataConfig)
